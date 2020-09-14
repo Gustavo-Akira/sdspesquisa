424288456741de199a77c5fc6954578f85c52700
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2020-09-14 12:27:25 -0300

    Project created

---
+++ backend/src/main/java/br/com/gustavoakira/devpesquisa/DevpesquisaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavoakira.devpesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevpesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevpesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/gustavoakira/devpesquisa/DevpesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavoakira.devpesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevpesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
